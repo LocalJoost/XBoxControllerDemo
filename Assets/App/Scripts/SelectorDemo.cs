4ac5b7abf71313e3344210d014d3517082d257d4
Joost van Schaik <joostvanschaik@outlook.com>
2018-04-29 17:43:45 +0200

    Initial code checkin

---
+++ Assets/App/Scripts/SelectorDemo.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkitExtensions.Utilities;
+using UnityEngine;
+
+public class SelectorDemo : MonoBehaviour, IInputClickHandler, IInputHandler    
+{
+    public Color SelectColor = Color.blue;
+
+    private DoubleClickPreventer _doubleClickPreventer = new DoubleClickPreventer(0.5f);
+
+
+    private Dictionary<MeshRenderer, Color> _originalColors;
+    // Use this for initialization
+    void Start()
+    {
+        _originalColors = new Dictionary<MeshRenderer, Color>();
+        SaveDefaultColors();
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        HandleClick();
+    }
+
+    public void OnInputDown(InputEventData eventData)
+    {
+        if (eventData.PressType == InteractionSourcePressInfo.Select)
+        {
+            HandleClick();
+        }
+    }
+
+    public void OnInputUp(InputEventData eventData)
+    {
+    }
+
+    private void HandleClick()
+    {
+        if (!_doubleClickPreventer.CanClick())
+        {
+            return;
+        }
+        Debug.Log("HandleClick");
+
+        StartCoroutine(FlashColor());
+        var audioSource = GetComponent<AudioSource>();
+        if (audioSource != null)
+        {
+            audioSource.Play();
+        }
+    }
+
+    private IEnumerator FlashColor()
+    {
+        SetColor(SelectColor);
+        yield return new WaitForSeconds(0.1f);
+        Reset();
+    }
+
+    private void SaveDefaultColors()
+    {
+        _originalColors.Clear();
+        foreach (var component in GetComponentsInChildren<MeshRenderer>())
+        {
+            _originalColors.Add(component, component.material.color);
+        }
+    }
+
+    private void SetColor(Color c)
+    {
+        foreach (var component in GetComponentsInChildren<MeshRenderer>())
+        {
+            component.material.color = c;
+        }
+    }
+
+    private void Reset()
+    {
+        foreach (var component in GetComponentsInChildren<MeshRenderer>())
+        {
+           component.material.color = _originalColors[component];
+        }
+    }
+}
---
+++ Assets/HoloToolkitExtensions/Utilities/DoubleClickPreventer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace HoloToolkitExtensions.Utilities
+{
+    public class DoubleClickPreventer
+    {
+        private readonly float _clickTimeOut;
+
+        private float _lastClick;
+
+        public DoubleClickPreventer(float clickTimeOut = 0.1f)
+        {
+            _clickTimeOut = clickTimeOut;
+        }
+
+        public bool CanClick()
+        {
+            if (!(Time.time - _lastClick > _clickTimeOut))
+            {
+                return false;
+            }
+            _lastClick = Time.time;
+            return true;
+        }
+    }
+}
